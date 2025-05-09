f5f1d6f0b5da49b5b5421d6fd6291d0bd796f4b2
ulrichPentz <ulrich.pentz1@gmail.com>
2025-05-09 10:40:56 +0200

    Chapter 10 exercises

---
+++ CHAPTER 10/file_exep.py
@@ -0,0 +1,166 @@
+from pathlib import Path
+import json
+
+print("Exercise 10.1")
+
+path=Path("reading_from_a_file/read.txt")
+contents=path.read_text()
+print(contents)
+
+lines=contents.splitlines()
+
+for line in lines:
+    print(line.replace(line))
+
+print("")
+#############################################
+print("Exercise 10.2")
+
+path=Path("reading_from_a_file/read.txt")
+contents=path.read_text()
+
+for line in contents.splitlines():
+    print(line.replace('Python', 'Java'))
+
+print("")
+#############################################
+print("Exercise 10.3")
+
+path=Path("reading_from_a_file/read.txt")
+contents=path.read_text()
+
+for line in contents.splitlines():
+    print(line.replace('Python', 'Java'))
+
+print("")
+############################################
+print("Exercise 10.4")
+
+name_input=input("Enter your name: \t")
+
+path = Path('guess.txt')
+path.write_text(name_input)
+
+print("")
+############################################
+print("Exercise 10.5")
+
+name_input=input("Enter your name (quit to exit): \t")
+path = Path('guess_book.txt')
+total_input=""
+active=True
+while active:
+    name_input=input("Enter another name (quit to exit): \t")
+    if name_input!="quit":
+        total_input+=f"{name_input}\n"
+    else:
+        active=False    
+
+path.write_text(total_input)
+
+print("")
+############################################
+print("Exercise 10.6")
+
+try:
+    number_1=float(input("Enter a number\t"))
+    number_2=float(input("Enter another number\t"))
+    print(number_1+number_2)
+except ValueError:
+    print("You didn't enter numbers")
+
+print("")
+############################################
+print("Exercise 10.7")
+answer=0
+while True:
+    first_number = input("Enter number: ")
+    if first_number == 'q':
+        break
+    try:
+        answer += float(first_number)
+    except ValueError:
+        print("you didn't add number")
+    print(answer)
+
+print("")
+############################################
+print("Exercise 10.8")
+cat_names=""
+path=Path("storing_data/cats.txt")
+cat_names+="Garfield\n"
+cat_names+="Freya\n"
+cat_names+="Maya"
+path.write_text(cat_names)
+
+dog_names=""
+path=Path("storing_data/dogs.txt")
+dog_names+="Spot\n"
+dog_names+="Max\n"
+dog_names+="Donner"
+path.write_text(dog_names)
+
+path1=Path("storing_data/cats1.txt")
+try:
+    contents=path1.read_text()
+    for line in contents.splitlines():
+        print(line)
+except FileNotFoundError:
+    print("No such file: cats1.txt")
+
+path2=Path("storing_data/dogs1.txt")
+try:
+    contents=path2.read_text()
+    for line in contents.splitlines():
+        print(line)
+except FileNotFoundError:
+    print("No such file: dogs1.txt")
+
+print("")
+############################################
+print("Exercise 10.9")
+
+path1=Path("storing_data/cats1.txt")
+try:
+    contents=path1.read_text()
+    for line in contents.splitlines():
+        print(line)
+except FileNotFoundError:
+    pass
+
+path2=Path("storing_data/dogs1.txt")
+try:
+    contents=path2.read_text()
+    for line in contents.splitlines():
+        print(line)
+except FileNotFoundError:
+    pass
+
+print("")
+#############################################
+print("Exercise 10.13")
+
+name=input("Enter name\t")
+surname=input("Enter surname\t")
+age=input("Enter age\t")
+
+person={
+    "name":name,
+    "surname":surname,
+    "age":age
+}
+
+path3=Path("storing_data/person.json")
+contents=json.dumps(person)
+path3.write_text(contents)
+
+path3=Path("storing_data/person.json")
+contents=path3.read_text()
+person1=json.loads(contents)
+
+for key, value in person1.items():
+    print(f"Key: {key}, Value: {value}")
+
+print(f"name: {person1['name']}")
+print(f"surname: {person1['surname']}")
+print(f"age: {person1['age']}")
--- CHAPTER 9/classes.py
+++ CHAPTER 9/classes.py
@@ -1,153 +1,153 @@
 from random import randint, choice
 
 ############################################################
-# print("Exercise 9.3")
+print("Exercise 9.3")
 
-# class User:
-#     def __init__(self, first_name, last_name, age, location, height):
-#         self.first_name=first_name
-#         self.last_name=last_name
-#         self.age=age
-#         self.location=location
-#         self.height=height
+class User:
+    def __init__(self, first_name, last_name, age, location, height):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.age=age
+        self.location=location
+        self.height=height
 
-#     def describe_self(self):
-#         print(f"Name: {self.first_name}")
-#         print(f"Surname: {self.last_name}")
-#         print(f"Height: {self.height}")
+    def describe_self(self):
+        print(f"Name: {self.first_name}")
+        print(f"Surname: {self.last_name}")
+        print(f"Height: {self.height}")
 
-#     def greet_user(self):
-#         print(f"Hello {self.first_name} {self.last_name}")
-
-
-# myself=User("Ulrich", "Pentz", "44", "Joburg", "1.91m")
-
-# myself.describe_self()
-# myself.greet_user()
-
-# print("")
-###################################################
-# print("Exercise 9.4")
-
-# class Restaurant:
-#     def __init__(self, name, cuisine):
-#         self.name=name
-#         self.cuisine=cuisine
-#         self.number_served=0
-
-#     def print_number_served(self):
-#         print(self.number_served)
-
-# new_restaurant=Restaurant("Joe's", "burgers")
-# new_restaurant.print_number_served()
-# new_restaurant.number_served=7
-# new_restaurant.print_number_served()
-# print("")
-############################################################
-# print("Exercise 9.5")
-
-# class User:
-#     def __init__(self, first_name, last_name, age, location, height, login_attempts):
-#         self.first_name=first_name
-#         self.last_name=last_name
-#         self.age=age
-#         self.location=location
-#         self.height=height
-#         self.login_attempts=login_attempts
-
-#     def increment_login_attempts(self):
-#         self.login_attempts+=1
-
-#     def reset_login_attempts(self):
-#         self.login_attempts=0
+    def greet_user(self):
+        print(f"Hello {self.first_name} {self.last_name}")
 
 
-# myself=User("Ulrich", "Pentz", "44", "Joburg", "1.91m", 0)
-# myself.increment_login_attempts()
-# myself.increment_login_attempts()
-# myself.increment_login_attempts()
-# print(myself.login_attempts)
-# myself.reset_login_attempts()
-# print(myself.login_attempts)
+myself=User("Ulrich", "Pentz", "44", "Joburg", "1.91m")
 
-# print("")
-############################################################
-# print("Exercise 9.6")
+myself.describe_self()
+myself.greet_user()
 
-# class IceCreamStand(Restaurant):
-#     def __init__(self, name, cuisine, flavour):
-#         super().__init__(name, cuisine)
-#         self.flavour=flavour
+print("")
+##################################################
+print("Exercise 9.4")
 
-#     def display_flavours(self):
-#         for flavour in self.flavour:
-#             print(flavour)
+class Restaurant:
+    def __init__(self, name, cuisine):
+        self.name=name
+        self.cuisine=cuisine
+        self.number_served=0
+
+    def print_number_served(self):
+        print(self.number_served)
+
+new_restaurant=Restaurant("Joe's", "burgers")
+new_restaurant.print_number_served()
+new_restaurant.number_served=7
+new_restaurant.print_number_served()
+print("")
+###########################################################
+print("Exercise 9.5")
+
+class User:
+    def __init__(self, first_name, last_name, age, location, height, login_attempts):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.age=age
+        self.location=location
+        self.height=height
+        self.login_attempts=login_attempts
+
+    def increment_login_attempts(self):
+        self.login_attempts+=1
+
+    def reset_login_attempts(self):
+        self.login_attempts=0
 
 
-# my_ice_cream=IceCreamStand("Corner Ice", "Ice Cream", ["choc", "strawberry", "vanilla"])
-# my_ice_cream.display_flavours()
+myself=User("Ulrich", "Pentz", "44", "Joburg", "1.91m", 0)
+myself.increment_login_attempts()
+myself.increment_login_attempts()
+myself.increment_login_attempts()
+print(myself.login_attempts)
+myself.reset_login_attempts()
+print(myself.login_attempts)
 
-# print("")
-############################################################
-# print("Exercise 9.13")
+print("")
+###########################################################
+print("Exercise 9.6")
 
-# class Die:
-#     def __init__(self):
-#         self.sides=6
+class IceCreamStand(Restaurant):
+    def __init__(self, name, cuisine, flavour):
+        super().__init__(name, cuisine)
+        self.flavour=flavour
 
-#     def roll_die(self):
-#         print(randint(1, self.sides))
+    def display_flavours(self):
+        for flavour in self.flavour:
+            print(flavour)
 
-# my_die=Die()
-# my_die.roll_die()
-# my_die.roll_die()
-# my_die.roll_die()
-# print("")
-# my_die.sides=10
-# my_die.roll_die()
-# my_die.roll_die()
-# my_die.roll_die()
-# print("")
-# my_die.sides=20
-# my_die.roll_die()
-# my_die.roll_die()
-# my_die.roll_die()
 
-# print("")
-############################################################
-# print("Exercise 9.14")    
+my_ice_cream=IceCreamStand("Corner Ice", "Ice Cream", ["choc", "strawberry", "vanilla"])
+my_ice_cream.display_flavours()
 
-# lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,"a", "b", "c", "d", "e"]
+print("")
+###########################################################
+print("Exercise 9.13")
 
-# element1=choice(lottery_elements)
-# element2=choice(lottery_elements)
-# element3=choice(lottery_elements)
-# element4=choice(lottery_elements)
-# element5=choice(lottery_elements)
+class Die:
+    def __init__(self):
+        self.sides=6
 
-# print(f"{element1} {element2} {element3} {element4} {element5}")
+    def roll_die(self):
+        print(randint(1, self.sides))
 
-# print("")
-############################################################
-# print("Exercise 9.14")    
+my_die=Die()
+my_die.roll_die()
+my_die.roll_die()
+my_die.roll_die()
+print("")
+my_die.sides=10
+my_die.roll_die()
+my_die.roll_die()
+my_die.roll_die()
+print("")
+my_die.sides=20
+my_die.roll_die()
+my_die.roll_die()
+my_die.roll_die()
 
-# lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,"a", "b", "c", "d", "e"]
+print("")
+###########################################################
+print("Exercise 9.14")    
 
-# element1=choice(lottery_elements)
-# element2=choice(lottery_elements)
-# element3=choice(lottery_elements)
-# element4=choice(lottery_elements)
-# element5=choice(lottery_elements)
+lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,"a", "b", "c", "d", "e"]
 
-# winning_ticket=f"{element1}{element2}{element3}{element4}{element5}"
+element1=choice(lottery_elements)
+element2=choice(lottery_elements)
+element3=choice(lottery_elements)
+element4=choice(lottery_elements)
+element5=choice(lottery_elements)
 
-# print(winning_ticket)
+print(f"{element1} {element2} {element3} {element4} {element5}")
 
-# print("")
+print("")
+###########################################################
+print("Exercise 9.14")    
+
+lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,"a", "b", "c", "d", "e"]
+
+element1=choice(lottery_elements)
+element2=choice(lottery_elements)
+element3=choice(lottery_elements)
+element4=choice(lottery_elements)
+element5=choice(lottery_elements)
+
+winning_ticket=f"{element1}{element2}{element3}{element4}{element5}"
+
+print(winning_ticket)
+
+print("")
 ############################################################
 print("Exercise 9.14")    
 winner="c789d"
-lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,"a", "b", "c", "d", "e"]
+lottery_elements=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "a", "b", "c", "d", "e"]
 winning_ticket=""
 counter=0
 def get_winner():
